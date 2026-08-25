c42dc0e8015989e29ca5ace800ea0a814c804ff0
lovetaoziyo <lovetaoziyo@gmail.com>
2026-08-26 01:54:09 +0800

    Fix API server: admin.rustdesk.com -> fxing.pathea.com:21114

--- rustdesk/src/common.rs
+++ rustdesk/src/common.rs
@@ -1082,7 +1082,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://fxing.pathea.com:21114".to_owned()
 }
 
 #[inline]
